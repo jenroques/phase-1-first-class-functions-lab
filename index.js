1cbc81f942869bd79123ef79b6716a9ccbefc77b
Jennifer Roques <jenniferroques@gmail.com>
2021-11-05 21:00:02 -0500

    commit tree 1e0eedb7a3e354fe2fe67fcaf898fa9cdc5fcbdf

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+   return drivers.slice(0,2);
+ }
+
+ const returnLastTwoDrivers = function(drivers) {
+     return drivers.slice(2,4);
+ }
+
+ let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ let createFareMultiplier = fareMultiplier => {
+     return function(rate) {
+        return rate * fareMultiplier; 
+     }
+    }
+
+const fareDoubler = createFareMultiplier(2)        
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+    
+
+
+    
